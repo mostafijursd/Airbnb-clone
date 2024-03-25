dcdaa42bf68791ee03ab3f03dce00e262fd077be
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-03-25 15:18:52 +0530

    change

---
+++ airbnb-clone/app/components/modals/Modals.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+interface ModalsProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+    secondaryAction?: () => void;
+    secondaryActionLabel?: string;
+  }
+const  Modals: React.FC<ModalsProps> = (
+{
+    isOpen, 
+  onClose, 
+  onSubmit, 
+  title, 
+  body, 
+  actionLabel, 
+  footer, 
+  disabled,
+  secondaryAction,
+  secondaryActionLabel
+}) => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default Modals
